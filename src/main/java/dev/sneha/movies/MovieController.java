429404884ddf8720e6b3089ee042278e7ebaed0f
snehasri16 <snehasariadupa145@gmail.com>
2026-03-05 17:55:19 +0530

    mongodb uri updation..

--- src/main/java/dev/sneha/movies/MovieController.java
+++ src/main/java/dev/sneha/movies/MovieController.java
@@ -3,12 +3,10 @@ package dev.sneha.movies;
 
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -21,9 +19,11 @@ public class MovieController {
     private MovieService movieService;
 
     @GetMapping
-    public ResponseEntity<List<Movie>> getAllMovies(){
-//        return new ResponseEntity<>("All Movies! :)..", HttpStatus.OK);
-        return  new ResponseEntity<List<Movie>>(movieService.allMovies(),HttpStatus.OK);
+    public ResponseEntity<Page<Movie>> getAllMovies(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size) {
+
+        return new ResponseEntity<>(movieService.getMovies(page, size), HttpStatus.OK);
     }
 
     @GetMapping("/{imdbId}")
--- src/main/java/dev/sneha/movies/MovieService.java
+++ src/main/java/dev/sneha/movies/MovieService.java
@@ -1,20 +1,22 @@
 package dev.sneha.movies;
 
-
-import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Optional;
 
 @Service
 public class MovieService {
+
     @Autowired
     public MovieRepository movieRepository;
 
-    public List<Movie> allMovies(){
-        return movieRepository.findAll();
+    public Page<Movie> getMovies(int page, int size){
+        Pageable pageable = PageRequest.of(page, size);
+        return movieRepository.findAll(pageable);
     }
 
     public Optional<Movie> singleMovie(String imdbId){
