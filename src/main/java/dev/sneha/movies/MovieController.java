d480da48dacc83909bb08142ff6b9a9ad0e6f6a9
snehasri16 <snehasariadupa145@gmail.com>
2026-03-05 13:34:52 +0530

    initial commit

---
+++ src/main/java/dev/sneha/movies/MovieController.java
@@ -0,0 +1,37 @@
+package dev.sneha.movies;
+
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("api/v1/movies")
+public class MovieController {
+
+    @Autowired
+    private MovieService movieService;
+
+    @GetMapping
+    public ResponseEntity<List<Movie>> getAllMovies(){
+//        return new ResponseEntity<>("All Movies! :)..", HttpStatus.OK);
+        return  new ResponseEntity<List<Movie>>(movieService.allMovies(),HttpStatus.OK);
+    }
+
+    @GetMapping("/{imdbId}")
+    public ResponseEntity<Movie> getSingleMovie(@PathVariable String imdbId){
+//        return new ResponseEntity<Optional<Movie>>(movieService.singleMovie(imdbId), HttpStatus.OK);
+
+        return movieService.singleMovie(imdbId)
+                .map(movie -> new ResponseEntity<>(movie, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+}
